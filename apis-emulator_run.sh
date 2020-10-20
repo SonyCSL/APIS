6e9b28846fc6eb7db0e7154041fb048ed788fdab
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-20 10:48:04 +0900

    Temporary Delete install related files.

--- apis-emulator_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd apis-emulator
-source apis-emulator/bin/activate
-python startEmul.py 4
--- apis-main_controller_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd apis-main_controller
-source apis-main_controller/bin/activate
-python startMain.py
